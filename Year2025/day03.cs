0476a334c79881f2bafa28d66541b8ba7439c48c
Emil Madsen <Nesstark0@gmail.com>
2025-12-03 13:10:17 +0100

    Day three part one and two fix

--- Year2025/day03.cs
+++ Year2025/day03.cs
@@ -7,74 +7,65 @@ class Program
     static void Main(string[] args)
     {
         string inputPath = Path.GetFullPath("inputs/day03.txt");
-
-        if (!File.Exists(inputPath))
-        {
-            Console.WriteLine("Input file not found: " + inputPath);
-            return;
-        }
-
         string[] lines = File.ReadAllLines(inputPath);
-
-        // ------------------------------
-        // Part One
-        // ------------------------------
+        
         long totalPartOne = 0;
-
-        foreach (string line in lines)
-        {
-            int bestTwoDigit = GetMaxJoltagePartOne(line);
-            totalPartOne += bestTwoDigit;
-        }
-
-        Console.WriteLine("Part One total joltage: " + totalPartOne);
-
-        // ------------------------------
-        // Part Two
-        // ------------------------------
         long totalPartTwo = 0;
-
+        
         foreach (string line in lines)
         {
-            long bestTwelveDigit = GetMaxJoltagePartTwo(line, 12); 
-            totalPartTwo += bestTwelveDigit;
+            totalPartOne += GetMaxJoltagePartOne(line);
+            totalPartTwo += GetMaxJoltagePartTwo(line, 12);
         }
-
+        
+        Console.WriteLine("Part One total joltage: " + totalPartOne);
         Console.WriteLine("Part Two total joltage: " + totalPartTwo);
     }
-
+    
     // --------------------------------------------------
     // Part one
     // --------------------------------------------------
     static int GetMaxJoltagePartOne(string bank)
     {
         int maxPair = -1;
-
+        
         for (int i = 0; i < bank.Length - 1; i++)
         {
             int a = bank[i] - '0';
-
-            for (int j = i + 1; j < bank.Length; j++)
+            
+            if (a == 9)
             {
-                int b = bank[j] - '0';
-                int value = a * 10 + b;
-
-                if (value > maxPair)
-                    maxPair = value;
+                for (int j = i + 1; j < bank.Length; j++)
+                {
+                    int b = bank[j] - '0';
+                    if (b == 9)
+                        return 99;
+                    maxPair = Math.Max(maxPair, a * 10 + b);
+                }
+            }
+            else
+            {
+                for (int j = i + 1; j < bank.Length; j++)
+                {
+                    int b = bank[j] - '0';
+                    int value = a * 10 + b;
+                    if (value > maxPair)
+                        maxPair = value;
+                }
             }
         }
-
+        
         return maxPair;
     }
-
+    
     // --------------------------------------------------
     // Part two
     // --------------------------------------------------
     static long GetMaxJoltagePartTwo(string bank, int k)
     {
         int removeCount = bank.Length - k;
-        var stack = new Stack<char>();
-
+        var stack = new Stack<char>(bank.Length);
+        
         foreach (char digit in bank)
         {
             while (stack.Count > 0 && removeCount > 0 && stack.Peek() < digit)
@@ -84,16 +75,16 @@ class Program
             }
             stack.Push(digit);
         }
-
+        
         while (removeCount > 0)
         {
             stack.Pop();
             removeCount--;
         }
-
+        
         char[] result = stack.ToArray();
         Array.Reverse(result);
-
-        return long.Parse(new string(result, 0, k));
+        
+        return long.Parse(result.AsSpan(0, k));
     }
 }
